645de00459a4b90bb68546ced4b6195af6ce7662
ArturoDuran90 <aduran@student.neumont.edu>
2023-01-11 21:18:01 -0700

    changed some things

--- reviewit/src/App.js
+++ reviewit/src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
 
 function App() {
+  const refreshPage = ()=>{
+		window.location.href='/';
+	}
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <nav>
+        This is the navabar
+      </nav>
+      <div>
+        please login 
+      </div>
+      <div>
+        <div>
+          name
+        </div>
+        <div>
+          last name
+        </div>
+        <div>
+          email
+        </div>
+        <div>
+          password
+        </div>
+        <div>
+          re-enter password
+        </div>
+        <div>
+          login
+        </div>
+        <span>
+          signup?
+        </span>
+      </div>
     </div>
   );
 }
