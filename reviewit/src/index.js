c7c0dd439cf2ca55e49da59aeaea81ee5cb466a5
ArturoDuran90 <aduran@student.neumont.edu>
2023-01-13 21:04:46 -0700

    Finished Login

--- reviewit/src/index.js
+++ reviewit/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './public/General.css';
 import LoginPg from './pages/LoginPg';
+import HomePg from './pages/HomePg';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginPg />
+    {/* <LoginPg /> */}
+    <HomePg />
   </React.StrictMode>
 );
 
--- reviewit/src/pages/LoginPg.js
+++ reviewit/src/pages/LoginPg.js
@@ -1,58 +1,27 @@
 import '../public/LoginPg.css';
 import logo from '../logo.png';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import SignupPg from './SignupPg';
-import HomePg from './HomePg';
-// import { NavLink } from "react-router-dom";
-
-
 
 function LoginPg() {
   return (
     <>
       <div id="container">
-      
-      
       <img id="loginLogo" src={logo} alt="logo"/>
-
         <div id="loginBox" className='box'>
           <h1 id="logSigText">Login</h1>
-  
           <div id='form'>
             <form>
               <label>Email:</label>
               <input type="text" name="Email"/><br/>  
               <label>Password:</label>
               <input type="text" name="Password"/><br/>
-              
               <br/>
               <input id="submitBtn" type="submit" value="Submit" />
-              <div >
-              Homepage
-                <a href={HomePg}/>
-              </div>
-              {/* </input> */}
             </form>
           </div>
-        
         </div>
         <span id="signInLink">
           Not a user? Sign Up 
         </span>
-        <div>
-        {/* <Router >
-          <NavLink id='lookUpBtn' to={`/SignupPg`}>
-            Login
-		      </NavLink>
-					<Routes>
-				    <Route path='/' exact element={<LoginPg/>} />
-            <Route path={'/SignupPg'} element={<SignupPg/>} />
-		    	</Routes>
-				</Router> */}
-        </div>
-        
-
-        
       </div>
     </>
   );
