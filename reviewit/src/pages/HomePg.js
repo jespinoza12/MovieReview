fba8b2fcb00daa151409aadebfbe07220f26682b
ArturoDuran90 <aduran@student.neumont.edu>
2023-01-13 22:08:39 -0700

    creating search page

--- reviewit/src/pages/HomePg.js
+++ reviewit/src/pages/HomePg.js
@@ -1,6 +1,30 @@
+import Navbar from "./Navbar";
+import "../public/HomePg.css";
+import background from "../background2.png"
+
 function HomePg() {
   return (
-    <p>Homepage</p>
+    <>
+    <Navbar />
+    <div id="container" style={{ 
+      backgroundImage: `url(${background})`,
+      backgorundRepeat: 'no-repeat',
+      width: '100%',
+      height: '92vh',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRrepeat: 'no-repeat'
+      }}>
+      <div>
+        <p id="search">Search</p>
+        <input type="search" placeholder="Search here"/>
+        <input id="submitBtn" type="submit" value="Submit" /> <br/>
+        <span id="AdvSrchLink">
+          Advanced Search 
+        </span>
+      </div>
+    </div>
+    </>
   );
 }
 
