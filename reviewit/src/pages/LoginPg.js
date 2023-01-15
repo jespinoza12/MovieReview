d8702ab04ab7f8dfc699bc51e87128d27f44de7f
jespinoza12 <jespinoza@student.neumont.edu>
2023-01-15 13:43:04 -0700

    Added the ability to get input data
    From login and register

--- reviewit/src/pages/LoginPg.js
+++ reviewit/src/pages/LoginPg.js
@@ -1,11 +1,21 @@
 import '../public/LoginPg.css';
 import logo from '../logo.png';
-
+import {useRef} from 'react';
 function LoginPg() {
 
   function register (){
     window.location.href='/register';
   }
+
+  const emailref = useRef();
+  const passwordref = useRef();
+
+  function onSubmit (e){
+    e.preventDefault();
+    console.log({email: emailref.current.value, password: passwordref.current.value});
+  }
+
+
 
   
 
@@ -16,11 +26,11 @@ function LoginPg() {
         <div id="loginBox" className='box'>
           <h1 id="logSigText">Login</h1>
           <div id='form'>
-            <form>
+            <form onSubmit={onSubmit}>
               <label>Email:</label>
-              <input type="text" name="Email"/><br/>  
+              <input ref={emailref} type="text" name="Email"/><br/>  
               <label>Password:</label>
-              <input type="text" name="Password"/><br/>
+              <input ref={passwordref} type="text" name="Password"/><br/>
               <br/>
               <input id="submitBtn" type="submit" value="Submit" />
             </form>
--- reviewit/src/pages/SignupPg.js
+++ reviewit/src/pages/SignupPg.js
@@ -1,10 +1,27 @@
 import '../public/SignupPg.css';
 import logo from '../logo.png';
-
+import {useRef} from 'react';
 function SignupPg() {
 
   const login = ()=>{
     window.location.href='/login';
+  }
+
+  const fname = useRef();
+  const lname = useRef();
+  const street = useRef();
+  const city = useRef();
+  const state = useRef();
+  const zip_code = useRef();
+  const email = useRef();
+  const phone = useRef();
+  const password = useRef();
+  const password2 = useRef();
+
+  function onSubmit (e){
+    e.preventDefault();
+    console.log({fname: fname.current.value, lname: lname.current.value, street: street.current.value, city: city.current.value, state: state.current.value, zip_code: zip_code.current.value, email: email.current.value, phone: phone.current.value, password: password.current.value, password2: password2.current.value});
+
   }
 
   return (
@@ -14,19 +31,29 @@ function SignupPg() {
         <div id="signupBox" className='box'>
           <h1 id="logSigText">Sign Up</h1>
           <div id='form'>
-            <form>
+            <form onSubmit={onSubmit}>
               <label>Fist Name:</label>
-              <input type="text" name="First Name"/><br/> 
+              <input ref={fname} type="text" name="First Name"/><br/> 
               <label>Last Name:</label>
-              <input type="text" name="Last Name"/><br/>   
+              <input ref={lname} type="text" name="Last Name"/><br/>   
+              <label>Street:</label>
+              <input ref={street} type="text" name="Street"/><br/>  
+              <label>City:</label>
+              <input ref={city} type="text" name="city"/><br/> 
+              <label>State</label>
+              <input ref={state} type="text" name="State"/><br/> 
+              <label>Zipcode:</label>
+              <input ref={zip_code} type="text" name="zipcode"/><br/> 
               <label>Email:</label>
-              <input type="text" name="Email"/><br/>  
+              <input ref={email} type="text" name="Email"/><br/>  
+              <label>Phone:</label>
+              <input ref={phone} type="text" name="Phone #"/><br/> 
               <label>Password:</label>
-              <input type="text" name="Password"/><br/>
+              <input ref={password} type="text" name="Password"/><br/>
               <label>Re-enter Password:</label>
-              <input type="text" name="Password"/><br/>
+              <input ref={password2} type="text" name="Password"/><br/>
               <br/>
-              <input id="submitBtn" type="submit" value="Submit" />
+              <button type='submit'>Submit</button>
             </form>
           </div>
         </div>
