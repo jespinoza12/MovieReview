65a306c2d912fc24fd432272b08b9134f3c5000d
ArturoDuran90 <aduran@student.neumont.edu>
2023-01-17 10:25:05 -0700

    added flexbox to review page
    still need to created, i just pushed, cuz i needed to restart my laptop

--- reviewit/src/pages/ReviewPg.js
+++ reviewit/src/pages/ReviewPg.js
@@ -1,17 +1,31 @@
 import Navbar from "./Navbar";
+import '../public/ReviewPg.css';
 
 function ReviewPg() {
   return(
     <>
       <Navbar />
-      <div id="container" className="box">
-        <div className="box">
-
+      <div id='container' class='box'>
+        <div id='box1' class='box'>Box 1</div>
+        <div id='box2' class='box'>
+            <div id= 'innerMenu1' class='box'>Menu 1</div>
+            <div id= 'innerMenu2' class='box'>Menu 2</div>
+            <div id= 'innerMenu3' class='box'>Menu 3</div>
+            <div id= 'innerMenu4' class='box'>Menu 4</div>
+            <div id= 'innerMenu5' class='box'>Menu 5</div>
         </div>
-      </div>
-    
-
-          
+        <div id='box3' class='box'>
+            <div id= 'menu1' class='box'>
+                <div id= 'innerMenu1' class='box'>Menu 1</div>
+                <div id= 'innerMenu2' class='box'>Menu 2</div>
+                <div id= 'innerMenu3' class='box'>Menu 3</div>
+                <div id= 'innerMenu4' class='box'>Menu 4</div>
+            </div>
+            <div id= 'menu2' class='box'>Box 4</div>
+            <div id= 'menu3' class='box'>Box 5</div>
+        </div>
+        <div id='box4' class='box'>Box 6</div>
+    </div>  
     </>
   );
 }
