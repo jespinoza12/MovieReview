33d0503bafaac9c5433a7666e8c66018dee2af1b
ArturoDuran90 <aduran@student.neumont.edu>
2023-01-19 09:12:34 -0700

    some more changes
    still need to work on the admin and review results pages

--- reviewit/src/pages/HomePg.js
+++ reviewit/src/pages/HomePg.js
@@ -15,12 +15,12 @@ function HomePg() {
       backgroundImage: `url(${background})`,
       backgorundRepeat: 'no-repeat',
       width: '100%',
-      height: '92vh',
+      height: '90vh',
       backgroundPosition: 'center',
       backgroundSize: 'cover',
       backgroundRrepeat: 'no-repeat'
       }}>
-      {/* <div id='searchBox'> */}
+      <div className='searchBox'>
         <div id="search">Search</div>
         {/* <label id="search">Search </label> */}
         <input type="search" placeholder="Search Movie by Title"/>
@@ -28,7 +28,7 @@ function HomePg() {
         <span id="AdvSrchLink" onClick={advancedSearch}>
           Advanced Search 
         </span>
-      {/* </div> */}
+      </div>
     </div>
     </>
   );
