acfa907dbe23f7b09712d7447fb1fbe63aa369be
ArturoDuran90 <aduran@student.neumont.edu>
2023-01-20 17:29:06 -0700

    deleted title from stars

--- reviewit/src/pages/StarsRating.js
+++ reviewit/src/pages/StarsRating.js
@@ -5,15 +5,15 @@ function StarRating(){
     <>
       <div class="rate">
         <input type="radio" id="star5" name="rate" value="5" />
-        <label for="star5" title="text"/>
+        <label for="star5"/>
         <input type="radio" id="star4" name="rate" value="4" />
-        <label for="star4" title="text"/>
+        <label for="star4"/>
         <input type="radio" id="star3" name="rate" value="3" />
-        <label for="star3" title="text"/>
+        <label for="star3"/>
         <input type="radio" id="star2" name="rate" value="2" />
-        <label for="star2" title="text"/>
+        <label for="star2"/>
         <input type="radio" id="star1" name="rate" value="1" />
-        <label for="star1" title="text"/>
+        <label for="star1"/>
       </div>
       <br/>
     </>
