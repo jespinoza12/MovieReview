4637d6033f9f9d64a26f13a05723b335e6fb38cb
jespinoza12 <jespinoza@student.neumont.edu>
2023-02-16 21:51:53 -0700

    Update sqlserver.js

--- sqlserver.js
+++ sqlserver.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const port = 9292;
+const port = process.env.PORT || 9002
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
