60c3ddc9b5b068d119c84019acc545ee69c9eb56
jespinoza12 <jespinoza@student.neumont.edu>
2023-02-16 23:15:58 -0700

    Fix hopefully

---
+++ client/src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./public/General.css";
+import LoginPg from "./pages/Home/LoginPg";
+import SignupPg from "./pages/Home/SignupPg";
+import HomePg from "./pages/Home/HomePg";
+import AdvSrchPg from "./pages/Search/AdvSrchPg";
+import SrchResPg from "./pages/Search/SrchResPg";
+import ReviewPg from "./pages/Reviews/ReviewPg";
+import ErrorPage from "./pages/Assets/Error-page";
+import AdvSrchResPg from "./pages/Search/AdvSrchResPg";
+import Admin from "./pages/Admin/Admin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import reportWebVitals from "./reportWebVitals";
+function App() {
+function isAdmin() {
+    if (localStorage.getItem("role") === "Admin") {
+      return true;
+    }else {
+      return false;
+    }
+  }
+  
+  function isLoggedIn() {
+    if (localStorage.getItem("token") !== null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  return (
+    <div>
+      <Router>
+        <Switch>
+        <Route exact path={process.env.PUBLIC_URL + "/"} fallback={<ErrorPage />}>
+            <HomePg />
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/searchResults"} fallback={<ErrorPage />}>
+            <SrchResPg />
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/register"} fallback={<ErrorPage />}>
+            <SignupPg />
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/login"} fallback={<ErrorPage />}>
+            <LoginPg />
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/advancedSearch"} fallback={<ErrorPage />}>
+            <AdvSrchPg />
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/advancedResults"} fallback={<ErrorPage />}>
+            <AdvSrchResPg />
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/movieResult"} fallback={<ErrorPage />}>
+            {isLoggedIn() ? <ReviewPg /> : <LoginPg/>}
+        </Route>
+        <Route exact path={process.env.PUBLIC_URL + "/admin"} fallback={<ErrorPage />}>
+            {isAdmin() ? <Admin /> : <HomePg />}
+        </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,90 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./public/General.css";
-import LoginPg from "./pages/Home/LoginPg";
-import SignupPg from "./pages/Home/SignupPg";
-import HomePg from "./pages/Home/HomePg";
-import AdvSrchPg from "./pages/Search/AdvSrchPg";
-import SrchResPg from "./pages/Search/SrchResPg";
-import ReviewPg from "./pages/Reviews/ReviewPg";
-import ErrorPage from "./pages/Assets/Error-page";
-import AdvSrchResPg from "./pages/Search/AdvSrchResPg";
-import Admin from "./pages/Admin/Admin";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import reportWebVitals from "./reportWebVitals";
-
-function isAdmin() {
-  if (localStorage.getItem("role") === "Admin") {
-    return true;
-  }else {
-    return false;
-  }
-}
-
-function isLoggedIn() {
-  if (localStorage.getItem("token") !== null) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePg />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/searchResults",
-    element: <SrchResPg />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/register",
-    element: <SignupPg />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPg />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/advancedSearch",
-    element: <AdvSrchPg />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/searchResults",
-    element: <AdvSrchPg />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/advancedResults",
-    element: <AdvSrchResPg />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/movieResult",
-    element: isLoggedIn() ? <ReviewPg /> : <LoginPg/>,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/admin",
-    element: isAdmin() ? <Admin /> : <HomePg />,
-    errorElement: <ErrorPage />,
-  },
-]);
-
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
-      <React.StrictMode>
-            <RouterProvider router={router} />
-      </React.StrictMode>
-    );
-    reportWebVitals();
-
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+)
   
 
