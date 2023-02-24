aaf2275048a079d9055e434417b264e30650fa33
jespinoza12 <jespinoza@student.neumont.edu>
2023-02-24 16:52:05 -0700

    fix

--- client/src/pages/Home/LoginPg.js
+++ client/src/pages/Home/LoginPg.js
@@ -36,7 +36,7 @@ const LoginPg = () => {
 
   const login = () => {
     axios
-      .post("http://localhost:9002/items/login", user)
+      .post("https://review-it.herokuapp.com/items/login", user)
       .then((res) => {
         if (res.data.token) {
           localStorage.setItem("token", res.data.token);
