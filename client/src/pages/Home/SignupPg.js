f1f19afd308c7377a6068db011e3bf51f9854810
jespinoza12 <jespinoza@student.neumont.edu>
2023-03-07 16:32:56 -0700

    Added form validation

--- client/src/pages/Home/SignupPg.js
+++ client/src/pages/Home/SignupPg.js
@@ -4,8 +4,10 @@ import { useState } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom"
 
+
 const SignupPg = () => {
   let history = useHistory()
+  const [errors, setErrors] = useState({});
 
   function home() {
     
@@ -31,47 +33,89 @@ const SignupPg = () => {
     password2: "",
   });
 
+  const validate = () => {
+    let errors = {};
+  
+    if (!user.fname.trim()) {
+      errors.fname = "First name is required";
+    }
+  
+    if (!user.lname.trim()) {
+      errors.lname = "Last name is required";
+    }
+  
+    if (!user.street.trim()) {
+      errors.street = "Street is required";
+    }
+  
+    if (!user.city.trim()) {
+      errors.city = "City is required";
+    }
+  
+    if (!user.state.trim()) {
+      errors.state = "State is required";
+    }
+  
+    if (!user.zip_code.trim()) {
+      errors.zip_code = "Zip code is required";
+    } else if (!/^\d{5}$/.test(user.zip_code)) {
+      errors.zip_code = "Zip code must be a 5-digit number";
+    }
+  
+    if (!user.email.trim()) {
+      errors.email = "Email is required";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(user.email)
+    ) {
+      errors.email = "Invalid email address";
+    }
+  
+    if (!user.phone.trim()) {
+      errors.phone = "Phone number is required";
+    } else if (!/^\d{10}$/.test(user.phone)) {
+      errors.phone = "Phone number must be a 10-digit number";
+    }
+  
+    if (!user.password.trim()) {
+      errors.password = "Password is required";
+    } else if (user.password.length < 8) {
+      errors.password = "Password must be at least 8 characters long";
+    }
+  
+    if (user.password !== user.password2) {
+      errors.password2 = "Passwords do not match";
+    }
+  
+    return errors;
+  };
+  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setUser({
       ...user,
       [name]: value,
     });
+    setErrors({
+      ...errors,
+      [name]: undefined,
+    });
   };
 
   const register = () => {
-    const {
-      fname,
-      lname,
-      street,
-      city,
-      state,
-      zip_code,
-      email,
-      phone,
-      password,
-      password2,
-    } = user;
-    if (
-      fname &&
-      lname &&
-      street &&
-      city &&
-      state &&
-      zip_code &&
-      phone &&
-      email &&
-      password &&
-      password === password2
-    ) {
-      axios.post("https://review-it.herokuapp.com/items/register", user).then((res) => {
+
+  const errors = validate();
+
+  if (Object.keys(errors).length > 0) {
+    setErrors(errors);
+  } else {
+    axios
+      .post("https://review-it.herokuapp.com/items/register", user)
+      .then((res) => {
         setMessage(res.data.message);
       });
-    } else {
-      setMessage("Oops");
-    }
-  };
-
+  }
+};
   function onSubmit(e) {
     e.preventDefault();
     register();
@@ -93,6 +137,7 @@ const SignupPg = () => {
                 type="text"
                 name="fname"
               />
+              {errors.fname ? <div className="error">{errors.fname}</div> : null}
               <br />
               <label>Last Name:</label>
               <input
@@ -101,6 +146,7 @@ const SignupPg = () => {
                 type="text"
                 name="lname"
               />
+              {errors.lname && <div className="error">{errors.lname}</div>}
               <br />
               <label>Street:</label>
               <input
@@ -109,6 +155,7 @@ const SignupPg = () => {
                 type="text"
                 name="street"
               />
+              {errors.street && <div className="error">{errors.street}</div>}
               <br />
               <label>City:</label>
               <input
@@ -117,6 +164,7 @@ const SignupPg = () => {
                 type="text"
                 name="city"
               />
+              {errors.city && <div className="error">{errors.city}</div>}
               <br />
               <label>State</label>
               <input
@@ -125,6 +173,7 @@ const SignupPg = () => {
                 type="text"
                 name="state"
               />
+              {errors.state && <div className="error">{errors.state}</div>}
               <br />
               <label>Zipcode:</label>
               <input
@@ -133,6 +182,7 @@ const SignupPg = () => {
                 type="text"
                 name="zip_code"
               />
+              {errors.zip_code && <div className="error">{errors.zip_code}</div>}
               <br />
               <label>Email:</label>
               <input
@@ -141,6 +191,7 @@ const SignupPg = () => {
                 type="text"
                 name="email"
               />
+              {errors.email && <div className="error">{errors.email}</div>}
               <br />
               <label>Phone:</label>
               <input
@@ -149,6 +200,7 @@ const SignupPg = () => {
                 type="text"
                 name="phone"
               />
+              {errors.phone && <div className="error">{errors.phone}</div>}
               <br />
               <label>Password:</label>
               <input
@@ -157,6 +209,7 @@ const SignupPg = () => {
                 type="text"
                 name="password"
               />
+              {errors.password && <div className="error">{errors.password}</div>}
               <br />
               <label>Re-enter Password:</label>
               <input
@@ -165,6 +218,7 @@ const SignupPg = () => {
                 type="text"
                 name="password2"
               />
+              {errors.password2 && <div className="error">{errors.password2}</div>}
               <br />
               <br />
               <input id="submitBtn" type="submit" value="Sign Up" />
